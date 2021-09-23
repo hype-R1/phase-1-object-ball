99411678f2c9431998885a8112f363e8782034fd
Sam Bencivengo <sambencivengo@gmail.com>
2021-09-23 10:55:24 -0400

    Object data filled with player Names

--- src/00-objectball.js
+++ src/00-objectball.js
@@ -3,15 +3,33 @@ function gameObject() {
     home: {
       teamName: "",
       colors: [],
-      players: {},
+      players: {
+        "Alan Anderson": {},
+        "Reggie Evans": {},
+        "Brook Lopez": {},
+        "Mason Plumlee": {},
+        "Jason Terry": {},
+      },
     },
     away: {
       teamName: "",
       colors: [],
-      players: {},
+      players: {
+        "Jeff Adrien": {},
+        "Bismak Biyombo": {},
+        "DeSagna Diop": {},
+        "Ben Gordon": {},
+        "Brendan Haywood": {},
+      },
     },
   };
-  return obj;
 }
 
-console.log(gameObject());
+// console.log(gameObject());
+
+// function homeTeamName() {
+//   let object = gameObject();
+//   return object["home"]["teamName"];
+// }
+
+// console.log(homeTeamName("Brooklyn Nets"));
